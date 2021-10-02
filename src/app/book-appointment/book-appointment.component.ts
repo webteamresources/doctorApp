36e5e240a1d98472c1d35ed8e90fa849bca80083
rupeshmestry <63701960+rupeshmestry@users.noreply.github.com>
2021-10-02 14:01:22 +0530

    upated

--- src/app/book-appointment/book-appointment.component.ts
+++ src/app/book-appointment/book-appointment.component.ts
@@ -28,9 +28,14 @@ export class BookAppointmentComponent implements OnInit {
 
 
   loadDocotrs() {
-    this.crud.getData('usersInfo?doctororpatient=Doctor').subscribe(
+    this.crud.getData('doctorsInfo.json').subscribe(
       (res: any) => {
-        this.doctorList = res
+        var data: any = Object.values(res);
+        data.forEach((item: any, i: number) => {
+          item.id = i;
+        });
+        let obj: any = data.find((x: any) => x.fullName);
+        this.doctorList.push({ id: obj.id, fullName: obj.fullName });
       },
       (err) => { }
     );
@@ -39,17 +44,31 @@ export class BookAppointmentComponent implements OnInit {
   fetchBooking() {
     var userEmail: any = this.ls.getData('useremail');
     //bookedUserEmail
-    this.crud.getData(`bookingInfo?bookedUserEmail=${userEmail}`).subscribe(
+    this.crud.getData(`bookingInfo.json`).subscribe(
       (res) => {
+        var data: any = Object.values(res);
+        let obj = data.find((x: any) => x.bookedUserEmail === userEmail);
         if (res > 0) {
           this.bookForm = true
-        } else {
-          this.bookingList = res;
+        }
+        else {
+          this.bookingList.push({ mobile: obj.mobile, patientName: obj.patientName, slotDate: obj.slotDate, slotTime: obj.slotTime });
           this.bookForm = false;
         }
       },
       (err) => { }
     )
+    // this.crud.getData(`bookingInfo?bookedUserEmail=${userEmail}`).subscribe(
+    //   (res) => {
+    //     if (res > 0) {
+    //       this.bookForm = true
+    //     } else {
+    //       this.bookingList = res;
+    //       this.bookForm = false;
+    //     }
+    //   },
+    //   (err) => { }
+    // )
   }
   newBooking() {
     this.bookForm = true;
@@ -64,29 +83,48 @@ export class BookAppointmentComponent implements OnInit {
       console.log(obj)
     } else {
       //bookingInfo?=docId=1&slotDate=2021-09-30&slotTime=10:00 AM to 11:00 AM
-
-      this.crud.getData(`bookingInfo?docId=${obj.docId}&slotDate=${obj.slotDate}&slotTime=${obj.slotTime}`).subscribe(
+      this.crud.getData(`bookingInfo.json`).subscribe(
         (res: any) => {
-          if (res.length > 0) {
+          //?docId=${obj.docId}&slotDate=${obj.slotDate}&slotTime=${obj.slotTime}
+          var data: any = Object.values(res);
+          if (obj.selectDoctor === data.docId && obj.slotDate === data.slotDate && obj.slotTime === data.slotTime) {
             this.msg = 'Please select another booking date and time'
           } else {
-            this.crud.postData('bookingInfo', obj).subscribe(
+            this.crud.postData('bookingInfo.json', obj).subscribe(
               (res: any) => {
+                this.bookingForm.reset();
+                this.bookForm = false;
+                this.msg = `Your Booking Date is ${obj.slotDate} and Time is ${obj.slotTime}`
                 console.log(res)
               },
               (err: any) => {
-                console.log(err)
+                this.msg = err
               }
             );
-            this.bookingForm.reset();
-            this.bookForm = false
-            this.msg = `Your Booking Date is ${obj.slotDate} and Time is ${obj.slotTime}`
           }
-        },
-        (err) => {
-          this.msg = err
-        }
-      )
+        });
+      // this.crud.getData(`bookingInfo?docId=${obj.docId}&slotDate=${obj.slotDate}&slotTime=${obj.slotTime}`).subscribe(
+      //   (res: any) => {
+      //     if (res.length > 0) {
+      //       this.msg = 'Please select another booking date and time'
+      //     } else {
+      //       this.crud.postData('bookingInfo', obj).subscribe(
+      //         (res: any) => {
+      //           console.log(res)
+      //         },
+      //         (err: any) => {
+      //           console.log(err)
+      //         }
+      //       );
+      //       this.bookingForm.reset();
+      //       this.bookForm = false
+      //       this.msg = `Your Booking Date is ${obj.slotDate} and Time is ${obj.slotTime}`
+      //     }
+      //   },
+      //   (err) => {
+      //     this.msg = err
+      //   }
+      // )
     }
   }
 
